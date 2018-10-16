cfd2b10be823c3ee1f6e6bbbcd2127e47c4a145d
osboxes.org <badric@hotmail.de>
2018-10-16 10:13:02 +0200

    pom changes and other fixes

---
+++ src/test/java/dummyTest.java
@@ -0,0 +1,25 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class dummyTest {
+
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+
+	
+	@Test
+	public void testShouldBeTrueDUMMYTEST() {
+		
+		Assert.assertEquals(2, 2);
+		Assert.assertNotSame(2,3);
+		
+	}
+	
+	
+}
